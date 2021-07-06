867d37266952b946380086f4c215caafaffde6e7
Dakota Whitney <whitneydakota@gmail.com>
2021-07-05 22:16:08 -0400

    Adding new profile images

--- app/js/main.js
+++ app/js/main.js
@@ -2,12 +2,12 @@ const burger = document.querySelector('.burger')
 const navbar = document.querySelector('#navbar')
 const main = document.querySelector('#main')
 
-function toggleNav(){
+function toggleNav() {
   navbar.classList.toggle('nav-active')
   main.classList.toggle('main-active')
   burger.classList.toggle('burger-active')
 }
 
-burger.addEventListener('click',function(){
+burger.addEventListener('click', function () {
   toggleNav();
 });
